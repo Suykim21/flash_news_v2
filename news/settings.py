87efc1aa38a32115d44cc49bea3de15416fd1b01
Steve Kim <suykim21@Steves-MacBook.local>
2017-11-09 21:45:46 -0800

    added settings stuff

--- news/settings.py
+++ news/settings.py
@@ -9,6 +9,20 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
 # EMAIL_BACKEND = django.core.mail.backends.console.EmailBackend
 
 #for gmail -- google apps
